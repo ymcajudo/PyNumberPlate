19fbb4322e10062f09e6a429f92b3f57193fa673
ymcajudo <ymcajudo@gmail.com>
2022-12-01 17:22:53 +0900

    updated

---
+++ ImgGet.py
@@ -0,0 +1,25 @@
+import os
+from PIL import Image
+
+# 다운받을 이미지 url
+sampleurl = "https://raw.githubusercontent.com/ymcajudo/PyNumberPlate/master/1.jpg"
+
+def imgdownload(url):
+    # curl 요청
+    # curl "이미지 주소" > "저장 될 이미지 파일 이름" 
+    tmpimgname = "test.jpg"
+    os.system("curl " + url + " > {}".format(tmpimgname))
+
+    """
+    # 저장 된 이미지 확인
+    curl_img = Image.open("./"+tmpimgname)
+    curl_img.show()
+    """
+
+    return tmpimgname
+    
+if __name__ == "__main__":
+    imgdownload(sampleurl)
+    #tmpimg = imgdownload(sampleurl)
+    #tmpimg.show()
+
--- NumPlateRecognition.py
+++ NumPlateRecognition.py
@@ -3,14 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pytesseract
 import time
+import ImgGet
 
 plt.style.use('dark_background')
 
+#github 내 샘플 자동차번호판 이미지. 이미지에 따라 인식률의 차이가 큼
+url = "https://raw.githubusercontent.com/ymcajudo/PyNumberPlate/master/1.jpg"
+
 def numplate(): 
     # 인식할 차량 번호판 선택
+    
     #img_ori = cv2.imread('1.jpg')              #본 python 스크립트를 실행하는 폴더에 파일이 있는 경우
-    img_path = 'D:/03.SW development/01.Python/01.Automatic Number Plate Recognition/1.jpg'
-    img_ori = cv2.imread(img_path)
+        
+    webimg = ImgGet.imgdownload(url)            #Web에서 img 받을 때
+    img_ori = cv2.imread(webimg)
+    
+    #img_path = 'D:/03.SW development/01.Python/01.Automatic Number Plate Recognition/1.jpg'    #local에 저장된 image의 path를 직접 지정
+    #img_ori = cv2.imread(img_path)
 
     height, width, channel = img_ori.shape
 
@@ -295,10 +304,20 @@ def numplate():
 
 
 if __name__ == "__main__":
+    
+    numplate()
+
+    """
     while True:
-        for i in range(0,5):
+        for i in range(0,5):            #exit after five(5) seconds
             numplate()
-            time.sleep(1)
+            time.sleep(1)               #time delay for one(1) second
         break
+    """
 
-
+    """
+    while True:
+        numplate()
+        time.sleep(1)
+    """
+    
